0dedea4e3c22a230a14945eb96d3ee215122f0de
0x1eef <0x1eef@protonmail.com>
2026-05-12 15:51:15 -0300

    style: expand one-liner ifs
    Every if/else block now uses multi-line format with
    braces on separate lines, matching the original
    process.c style.

--- src/process.c
+++ src/process.c
@@ -51,7 +51,9 @@ static int
 executable_exists(const char *path)
 {
   struct stat st;
-  if (stat(path, &st) == -1) return 0;
+  if (stat(path, &st) == -1) {
+    return 0;
+  }
   return (st.st_mode & S_IXUSR) ? 1 : 0;
 }
 
@@ -67,19 +69,29 @@ search_path(const char *cmd)
   size_t cmd_len, dir_len;
 
   path_env = getenv("PATH");
-  if (!path_env) return NULL;
+  if (!path_env) {
+    return NULL;
+  }
   path_copy = strdup(path_env);
-  if (!path_copy) return NULL;
+  if (!path_copy) {
+    return NULL;
+  }
   cmd_len = strlen(cmd);
   dir = strtok(path_copy, ":");
   while (dir) {
     dir_len = strlen(dir);
     full_path = (char *)malloc(dir_len + 1 + cmd_len + 1);
-    if (!full_path) { free(path_copy); return NULL; }
+    if (!full_path) {
+      free(path_copy);
+      return NULL;
+    }
     memcpy(full_path, dir, dir_len);
     full_path[dir_len] = '/';
     memcpy(full_path + dir_len + 1, cmd, cmd_len + 1);
-    if (executable_exists(full_path)) { free(path_copy); return full_path; }
+    if (executable_exists(full_path)) {
+      free(path_copy);
+      return full_path;
+    }
     free(full_path);
     dir = strtok(NULL, ":");
   }
@@ -95,10 +107,16 @@ static void
 check_command(mrb_state *mrb, const char *cmd)
 {
   if (strchr(cmd, '/')) {
-    if (!executable_exists(cmd)) { errno = ENOENT; mrb_sys_fail(mrb, cmd); }
+    if (!executable_exists(cmd)) {
+      errno = ENOENT;
+      mrb_sys_fail(mrb, cmd);
+    }
   } else {
     char *found = search_path(cmd);
-    if (!found) { errno = ENOENT; mrb_sys_fail(mrb, cmd); }
+    if (!found) {
+      errno = ENOENT;
+      mrb_sys_fail(mrb, cmd);
+    }
     free(found);
   }
 }
@@ -111,7 +129,9 @@ static void
 redirect_fd(mrb_state *mrb, int src, int dest)
 {
   if (src != dest) {
-    if (dup2(src, dest) == -1) { mrb_sys_fail(mrb, "dup2 failed"); }
+    if (dup2(src, dest) == -1) {
+      mrb_sys_fail(mrb, "dup2 failed");
+    }
     close(src);
   }
 }
@@ -122,10 +142,14 @@ redirect_fd(mrb_state *mrb, int src, int dest)
 static int
 fd_from_value(mrb_state *mrb, mrb_value val)
 {
-  if (mrb_fixnum_p(val)) return mrb_fixnum(val);
+  if (mrb_fixnum_p(val)) {
+    return mrb_fixnum(val);
+  }
   if (mrb_respond_to(mrb, val, mrb_intern_lit(mrb, "to_i"))) {
     val = mrb_funcall(mrb, val, "to_i", 0);
-    if (mrb_fixnum_p(val)) return mrb_fixnum(val);
+    if (mrb_fixnum_p(val)) {
+      return mrb_fixnum(val);
+    }
   }
   return -1;
 }
@@ -151,60 +175,106 @@ mrb_f_spawn(mrb_state *mrb, mrb_value klass)
   int out_fd = -1, err_fd = -1, in_fd = -1;
 
   mrb_get_args(mrb, "*", &argv, &argc);
-  if (argc == 0)
+  if (argc == 0) {
     mrb_raise(mrb, E_ARGUMENT_ERROR, "wrong number of arguments (0 for 1+)");
-
-  if (argc > 0 && mrb_hash_p(argv[argc - 1])) { opts = argv[argc - 1]; argc--; }
-
+  }
+  if (argc > 0 && mrb_hash_p(argv[argc - 1])) {
+    opts = argv[argc - 1];
+    argc--;
+  }
   if (!mrb_nil_p(opts)) {
     mrb_value out_val, err_val, in_val;
     out_val = mrb_hash_get(mrb, opts, mrb_symbol_value(mrb_intern_lit(mrb, "out")));
-    if (!mrb_nil_p(out_val)) { out_fd = fd_from_value(mrb, out_val); if (out_fd < 0) mrb_raise(mrb, E_ARGUMENT_ERROR, "bad out option"); }
+    if (!mrb_nil_p(out_val)) {
+      out_fd = fd_from_value(mrb, out_val);
+      if (out_fd < 0) {
+        mrb_raise(mrb, E_ARGUMENT_ERROR, "bad out option");
+      }
+    }
     err_val = mrb_hash_get(mrb, opts, mrb_symbol_value(mrb_intern_lit(mrb, "err")));
-    if (!mrb_nil_p(err_val)) { err_fd = fd_from_value(mrb, err_val); if (err_fd < 0) mrb_raise(mrb, E_ARGUMENT_ERROR, "bad err option"); }
+    if (!mrb_nil_p(err_val)) {
+      err_fd = fd_from_value(mrb, err_val);
+      if (err_fd < 0) {
+        mrb_raise(mrb, E_ARGUMENT_ERROR, "bad err option");
+      }
+    }
     in_val = mrb_hash_get(mrb, opts, mrb_symbol_value(mrb_intern_lit(mrb, "in")));
-    if (!mrb_nil_p(in_val)) { in_fd = fd_from_value(mrb, in_val); if (in_fd < 0) mrb_raise(mrb, E_ARGUMENT_ERROR, "bad in option"); }
+    if (!mrb_nil_p(in_val)) {
+      in_fd = fd_from_value(mrb, in_val);
+      if (in_fd < 0) {
+        mrb_raise(mrb, E_ARGUMENT_ERROR, "bad in option");
+      }
+    }
   }
-
   if (mrb_array_p(argv[0])) {
-    mrb_value *arr; mrb_int arr_len;
-    arr = RARRAY_PTR(argv[0]); arr_len = RARRAY_LEN(argv[0]);
-    if (arr_len < 1) mrb_raise(mrb, E_ARGUMENT_ERROR, "first element of array must be a command");
-    prog = arr[0]; prog0 = (arr_len > 1) ? arr[1] : prog; start = 1;
+    mrb_value *arr;
+    mrb_int arr_len;
+    arr = RARRAY_PTR(argv[0]);
+    arr_len = RARRAY_LEN(argv[0]);
+    if (arr_len < 1) {
+      mrb_raise(mrb, E_ARGUMENT_ERROR, "first element of array must be a command");
+    }
+    prog = arr[0];
+    if (arr_len > 1) {
+      prog0 = arr[1];
+    } else {
+      prog0 = prog;
+    }
+    start = 1;
     check_command(mrb, mrb_string_value_cstr(mrb, &prog));
   } else {
-    prog = argv[0]; prog0 = argv[0];
+    prog = argv[0];
+    prog0 = argv[0];
     if (argc == 1) {
       pid = fork();
       if (pid == 0) {
-        if (out_fd >= 0) redirect_fd(mrb, out_fd, STDOUT_FILENO);
-        if (err_fd >= 0) redirect_fd(mrb, err_fd, STDERR_FILENO);
-        if (in_fd >= 0) redirect_fd(mrb, in_fd, STDIN_FILENO);
+        if (out_fd >= 0) {
+          redirect_fd(mrb, out_fd, STDOUT_FILENO);
+        }
+        if (err_fd >= 0) {
+          redirect_fd(mrb, err_fd, STDERR_FILENO);
+        }
+        if (in_fd >= 0) {
+          redirect_fd(mrb, in_fd, STDIN_FILENO);
+        }
         execl("/bin/sh", "sh", "-c", mrb_string_value_cstr(mrb, &argv[0]), (char *)NULL);
         _exit(127);
       }
-      if (pid < 0) mrb_sys_fail(mrb, "fork failed");
+      if (pid < 0) {
+        mrb_sys_fail(mrb, "fork failed");
+      }
       return mrb_fixnum_value(pid);
     }
     start = 1;
     check_command(mrb, mrb_string_value_cstr(mrb, &prog));
   }
-
   {
-    const char **cargv = (const char **)mrb_alloca(mrb, sizeof(char *) * (argc - start + 2));
+    const char **cargv;
+    cargv = (const char **)mrb_alloca(mrb, sizeof(char *) * (argc - start + 2));
     cargv[0] = mrb_string_value_cstr(mrb, &prog0);
-    for (i = start; i < argc; i++) cargv[i - start + 1] = mrb_string_value_cstr(mrb, &argv[i]);
+    for (i = start; i < argc; i++) {
+      cargv[i - start + 1] = mrb_string_value_cstr(mrb, &argv[i]);
+    }
     cargv[argc - start + 1] = NULL;
-    fflush(stdout); fflush(stderr);
+    fflush(stdout);
+    fflush(stderr);
     pid = fork();
     if (pid == 0) {
-      if (out_fd >= 0) redirect_fd(mrb, out_fd, STDOUT_FILENO);
-      if (err_fd >= 0) redirect_fd(mrb, err_fd, STDERR_FILENO);
-      if (in_fd >= 0) redirect_fd(mrb, in_fd, STDIN_FILENO);
+      if (out_fd >= 0) {
+        redirect_fd(mrb, out_fd, STDOUT_FILENO);
+      }
+      if (err_fd >= 0) {
+        redirect_fd(mrb, err_fd, STDERR_FILENO);
+      }
+      if (in_fd >= 0) {
+        redirect_fd(mrb, in_fd, STDIN_FILENO);
+      }
       execvp(mrb_string_value_cstr(mrb, &prog), cargv);
       _exit(errno == ENOENT ? 127 : 126);
     }
-    if (pid < 0) mrb_sys_fail(mrb, "fork failed");
+    if (pid < 0) {
+      mrb_sys_fail(mrb, "fork failed");
+    }
     return mrb_fixnum_value(pid);
   }
 }
@@ -227,26 +297,43 @@ mrb_f_kill(mrb_state *mrb, mrb_value klass)
   if (mrb_fixnum_p(sigo)) {
     signo = mrb_fixnum(sigo);
   } else if (mrb_string_p(sigo) || mrb_symbol_p(sigo)) {
-    if (mrb_string_p(sigo)) { name = RSTRING_PTR(sigo); namelen = (size_t)RSTRING_LEN(sigo); }
-    else { name = mrb_sym2name_len(mrb, mrb_symbol(sigo), &namelen); }
-    if (namelen >= 3 && strncmp(name, "SIG", 3) == 0) { name += 3; namelen -= 3; }
+    if (mrb_string_p(sigo)) {
+      name = RSTRING_PTR(sigo);
+      namelen = (size_t)RSTRING_LEN(sigo);
+    } else {
+      name = mrb_sym2name_len(mrb, mrb_symbol(sigo), &namelen);
+    }
+    if (namelen >= 3 && strncmp(name, "SIG", 3) == 0) {
+      name += 3;
+      namelen -= 3;
+    }
     for (i = 0; signals[i].name != NULL; i++) {
       symlen = strlen(signals[i].name);
-      if (symlen == namelen && strncmp(name, signals[i].name, symlen) == 0) { signo = signals[i].no; break; }
+      if (symlen == namelen && strncmp(name, signals[i].name, symlen) == 0) {
+        signo = signals[i].no;
+        break;
+      }
     }
-    if (signals[i].name == NULL)
+    if (signals[i].name == NULL) {
       mrb_raisef(mrb, E_ARGUMENT_ERROR, "unsupported name `SIG%S'", mrb_str_new(mrb, name, namelen));
+    }
   } else {
     mrb_raisef(mrb, E_TYPE_ERROR, "bad signal type %S", mrb_obj_value(mrb_class(mrb, sigo)));
   }
-
   sent = 0;
-  if (kill(pid, signo) == -1) mrb_sys_fail(mrb, "kill");
+  if (kill(pid, signo) == -1) {
+    mrb_sys_fail(mrb, "kill");
+  }
   sent++;
   while (argc-- > 0) {
-    if (!mrb_fixnum_p(*argv)) mrb_raisef(mrb, E_TYPE_ERROR, "wrong argument type %S (expected Fixnum)", mrb_obj_value(mrb_class(mrb, *argv)));
-    if (kill(mrb_fixnum(*argv), signo) == -1) mrb_sys_fail(mrb, "kill");
-    sent++; argv++;
+    if (!mrb_fixnum_p(*argv)) {
+      mrb_raisef(mrb, E_TYPE_ERROR, "wrong argument type %S (expected Fixnum)", mrb_obj_value(mrb_class(mrb, *argv)));
+    }
+    if (kill(mrb_fixnum(*argv), signo) == -1) {
+      mrb_sys_fail(mrb, "kill");
+    }
+    sent++;
+    argv++;
   }
   return mrb_fixnum_value(sent);
 }
@@ -254,15 +341,23 @@ mrb_f_kill(mrb_state *mrb, mrb_value klass)
 static mrb_value
 mrb_f_fork(mrb_state *mrb, mrb_value klass)
 {
-  mrb_value b; int pid;
+  mrb_value b;
+  int pid;
+
   mrb_get_args(mrb, "&", &b);
   switch (pid = fork()) {
   case 0:
     mrb_gv_set(mrb, mrb_intern_lit(mrb, "$$"), mrb_fixnum_value((mrb_int)getpid()));
-    if (!mrb_nil_p(b)) { mrb_yield_argv(mrb, b, 0, NULL); _exit(0); }
+    if (!mrb_nil_p(b)) {
+      mrb_yield_argv(mrb, b, 0, NULL);
+      _exit(0);
+    }
     return mrb_nil_value();
-  case -1: mrb_sys_fail(mrb, "fork failed"); return mrb_nil_value();
-  default: return mrb_fixnum_value(pid);
+  case -1:
+    mrb_sys_fail(mrb, "fork failed");
+    return mrb_nil_value();
+  default:
+    return mrb_fixnum_value(pid);
   }
 }
 
@@ -270,19 +365,32 @@ static int
 mrb_waitpid(int pid, int flags, int *st)
 {
   int result;
+
 retry:
   result = waitpid(pid, st, flags);
-  if (result < 0) { if (errno == EINTR) goto retry; return -1; }
+  if (result < 0) {
+    if (errno == EINTR) {
+      goto retry;
+    }
+    return -1;
+  }
   return result;
 }
 
 static mrb_value
 mrb_f_waitpid(mrb_state *mrb, mrb_value klass)
 {
-  mrb_int pid, flags = 0; int status;
+  mrb_int pid, flags = 0;
+  int status;
+
   mrb_get_args(mrb, "i|i", &pid, &flags);
-  if ((pid = mrb_waitpid(pid, flags, &status)) < 0) mrb_sys_fail(mrb, "waitpid failed");
-  if (!pid && (flags & WNOHANG)) { mrb_gv_set(mrb, mrb_intern_lit(mrb, "$?"), mrb_nil_value()); return mrb_nil_value(); }
+  if ((pid = mrb_waitpid(pid, flags, &status)) < 0) {
+    mrb_sys_fail(mrb, "waitpid failed");
+  }
+  if (!pid && (flags & WNOHANG)) {
+    mrb_gv_set(mrb, mrb_intern_lit(mrb, "$?"), mrb_nil_value());
+    return mrb_nil_value();
+  }
   mrb_gv_set(mrb, mrb_intern_lit(mrb, "$?"), mrb_procstat_new(mrb, pid, status));
   return mrb_fixnum_value(pid);
 }
@@ -290,18 +398,32 @@ mrb_f_waitpid(mrb_state *mrb, mrb_value klass)
 mrb_value
 mrb_f_sleep(mrb_state *mrb, mrb_value klass)
 {
-  mrb_int argc; mrb_value *argv; time_t beg, end;
+  mrb_int argc;
+  mrb_value *argv;
+  time_t beg, end;
+
   beg = time(0);
   mrb_get_args(mrb, "*", &argv, &argc);
   if (argc == 0) {
     sleep((32767<<16)+32767);
   } else if (argc == 1) {
-    struct timeval tv; int n;
-    if (mrb_fixnum_p(argv[0])) { tv.tv_sec = mrb_fixnum(argv[0]); tv.tv_usec = 0; }
-    else { tv.tv_sec = mrb_float(argv[0]); tv.tv_usec = (mrb_float(argv[0]) - tv.tv_sec) * 1000000.0; }
+    struct timeval tv;
+    int n;
+
+    if (mrb_fixnum_p(argv[0])) {
+      tv.tv_sec = mrb_fixnum(argv[0]);
+      tv.tv_usec = 0;
+    } else {
+      tv.tv_sec = mrb_float(argv[0]);
+      tv.tv_usec = (mrb_float(argv[0]) - tv.tv_sec) * 1000000.0;
+    }
     n = select(0, 0, 0, 0, &tv);
-    if (n < 0) mrb_sys_fail(mrb, "mrb_f_sleep failed");
-  } else { mrb_raise(mrb, E_ARGUMENT_ERROR, "wrong # of arguments"); }
+    if (n < 0) {
+      mrb_sys_fail(mrb, "mrb_f_sleep failed");
+    }
+  } else {
+    mrb_raise(mrb, E_ARGUMENT_ERROR, "wrong # of arguments");
+  }
   end = time(0) - beg;
   return mrb_fixnum_value(end);
 }
@@ -309,42 +431,83 @@ mrb_f_sleep(mrb_state *mrb, mrb_value klass)
 mrb_value
 mrb_f_system(mrb_state *mrb, mrb_value klass)
 {
-  int ret; mrb_value *argv, pname; const char *path; mrb_int argc; void (*chfunc)(int);
-  fflush(stdout); fflush(stderr);
+  int ret;
+  mrb_value *argv, pname;
+  const char *path;
+  mrb_int argc;
+  void (*chfunc)(int);
+
+  fflush(stdout);
+  fflush(stderr);
   mrb_get_args(mrb, "*", &argv, &argc);
-  if (argc == 0) mrb_raise(mrb, E_ARGUMENT_ERROR, "wrong number of arguments");
-  pname = argv[0]; path = mrb_string_value_cstr(mrb, &pname);
-  chfunc = signal(SIGCHLD, SIG_DFL); ret = system(path); signal(SIGCHLD, chfunc);
-  if (WIFEXITED(ret) && WEXITSTATUS(ret) == 0) return mrb_true_value();
+  if (argc == 0) {
+    mrb_raise(mrb, E_ARGUMENT_ERROR, "wrong number of arguments");
+  }
+  pname = argv[0];
+  path = mrb_string_value_cstr(mrb, &pname);
+  chfunc = signal(SIGCHLD, SIG_DFL);
+  ret = system(path);
+  signal(SIGCHLD, chfunc);
+  if (WIFEXITED(ret) && WEXITSTATUS(ret) == 0) {
+    return mrb_true_value();
+  }
   return mrb_false_value();
 }
 
 mrb_value
-mrb_f_exit(mrb_state *mrb, mrb_value klass) { return mrb_f_exit_common(mrb, 0); }
+mrb_f_exit(mrb_state *mrb, mrb_value klass)
+{
+  return mrb_f_exit_common(mrb, 0);
+}
+
 mrb_value
-mrb_f_exit_bang(mrb_state *mrb, mrb_value klass) { return mrb_f_exit_common(mrb, 1); }
+mrb_f_exit_bang(mrb_state *mrb, mrb_value klass)
+{
+  return mrb_f_exit_common(mrb, 1);
+}
 
 static mrb_value
 mrb_f_exit_common(mrb_state *mrb, int bang)
 {
-  mrb_value status; int istatus, n;
+  mrb_value status;
+  int istatus, n;
+
   n = mrb_get_args(mrb, "|o", &status);
-  if (n == 0) status = (bang) ? mrb_false_value() : mrb_true_value();
-  if (mrb_type(status) == MRB_TT_TRUE) istatus = EXIT_SUCCESS;
-  else if (mrb_type(status) == MRB_TT_FALSE) istatus = EXIT_FAILURE;
-  else { status = mrb_convert_type(mrb, status, MRB_TT_FIXNUM, "Integer", "to_int"); istatus = mrb_fixnum(status); }
-  if (bang) _exit(istatus); else exit(istatus);
+  if (n == 0) {
+    status = (bang) ? mrb_false_value() : mrb_true_value();
+  }
+  if (mrb_type(status) == MRB_TT_TRUE) {
+    istatus = EXIT_SUCCESS;
+  } else if (mrb_type(status) == MRB_TT_FALSE) {
+    istatus = EXIT_FAILURE;
+  } else {
+    status = mrb_convert_type(mrb, status, MRB_TT_FIXNUM, "Integer", "to_int");
+    istatus = mrb_fixnum(status);
+  }
+  if (bang) {
+    _exit(istatus);
+  } else {
+    exit(istatus);
+  }
 }
 
 mrb_value
-mrb_f_pid(mrb_state *mrb, mrb_value klass) { return mrb_fixnum_value((mrb_int)getpid()); }
+mrb_f_pid(mrb_state *mrb, mrb_value klass)
+{
+  return mrb_fixnum_value((mrb_int)getpid());
+}
+
 mrb_value
-mrb_f_ppid(mrb_state *mrb, mrb_value klass) { return mrb_fixnum_value((mrb_int)getppid()); }
+mrb_f_ppid(mrb_state *mrb, mrb_value klass)
+{
+  return mrb_fixnum_value((mrb_int)getppid());
+}
 
 static mrb_value
 mrb_procstat_new(mrb_state *mrb, mrb_int pid, mrb_int status)
 {
   struct RClass *cls;
+
   cls = mrb_class_get_under(mrb, mrb_module_get(mrb, "Process"), "Status");
   return mrb_funcall(mrb, mrb_obj_value(cls), "new", 2, mrb_fixnum_value(pid), mrb_fixnum_value(status));
 }
@@ -364,7 +527,9 @@ static mrb_value
 mrb_procstat_exitstatus(mrb_state *mrb, mrb_value self)
 {
   int i = mrb_fixnum(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@status")));
-  if (WIFEXITED(i)) return mrb_fixnum_value(WEXITSTATUS(i));
+  if (WIFEXITED(i)) {
+    return mrb_fixnum_value(WEXITSTATUS(i));
+  }
   return mrb_nil_value();
 }
 
@@ -393,7 +558,9 @@ static mrb_value
 mrb_procstat_stopsig(mrb_state *mrb, mrb_value self)
 {
   int i = mrb_fixnum(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@status")));
-  if (WIFSTOPPED(i)) return mrb_fixnum_value(WSTOPSIG(i));
+  if (WIFSTOPPED(i)) {
+    return mrb_fixnum_value(WSTOPSIG(i));
+  }
   return mrb_nil_value();
 }
 
@@ -401,7 +568,9 @@ static mrb_value
 mrb_procstat_termsig(mrb_state *mrb, mrb_value self)
 {
   int i = mrb_fixnum(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@status")));
-  if (WIFSIGNALED(i)) return mrb_fixnum_value(WTERMSIG(i));
+  if (WIFSIGNALED(i)) {
+    return mrb_fixnum_value(WTERMSIG(i));
+  }
   return mrb_nil_value();
 }
 
